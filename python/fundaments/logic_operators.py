d4796a388ab346400064a6919b40b5ca0a458438
Raul Franck <raulfranck@companyhero.com>
2024-02-18 20:43:44 -0300

    add logic operators with python

---
+++ python/fundaments/logic_operators.py
@@ -0,0 +1,23 @@
+
+'''
+    Logic operators with Python
+'''
+
+years_old = 20
+height = 1.80
+
+# AND is a operator like && with javascript
+result = years_old >= 30 and height >= 1.80
+
+print(result)
+
+# OR is a operator like || in javascript
+
+result2 = years_old >= 30 or height >= 1.80
+print(result2)
+
+# NOT is a operator like ! in javascript
+
+has_money = True
+
+print(not has_money) # Flase
