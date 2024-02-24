e3bda3b851d64d491d50bcdddc747bc941502022
Raul Franck <raulfranck@companyhero.com>
2024-02-24 09:28:37 -0300

    Learn about class atribute and instance atribute

--- python/oop/first_class.py
+++ python/oop/first_class.py
@@ -1,5 +1,7 @@
 
 class Item:
+    pay_rate = 0.8
+    
     def __init__(self, name, price, quantity):
         # Run validations to the received arguments
         assert price >= 0, f"Price {price} is not greater than or equal to zero"
@@ -9,25 +11,25 @@ class Item:
         self.name = name
         self.price = price
         self.quantity = quantity
-        print(f"An instance created: {name} {price} {quantity}")
+        # print(f"An instance created: {name} {price} {quantity}")
         
     def calculate_total_price(self):
         return self.price * self.quantity
-        # return x * y
+        
+    def apply_discount(self):
+         self.price = self.price * self.pay_rate
+         return self.price
 
-item1 = Item(name="Phone", price=10, quantity=5)
-# item1.name = "Phone"
-# item1.price = 100
-# item1.quantity = 5
+item1 = Item(name="Phone", price=1000, quantity=5)
+item2 = Item(name="Laptop", price=1000, quantity=2)
+item3 = Item(name="AirFryer", price=1000, quantity=10)
 
-item2 = Item("Laptop", 1000, 3) 
-# item2.name = "Laptop"
-# item2.price = 1000
-# item2.quantity = 3
+# Apply different discount to each item
+item2.pay_rate = 0.7
+item3.pay_rate = 0.6
 
-print(item1.calculate_total_price())
-print(item2.calculate_total_price())
+print(item1.apply_discount())
+print(item2.apply_discount())
+print(item3.apply_discount())
 
-# print(item1.calculate_total_price(item1.price, item1.quantity)) As passing params
-# print(item2.calculate_total_price(item2.price, item2.quantity)) As passing params
 
